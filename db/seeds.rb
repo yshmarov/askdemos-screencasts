d6a0f60b589c621f3bc6184cb7b1915a8ecbbe32
Yaro <yshmarov@gmail.com>
2021-11-06 16:21:00 +0000

    faker, seeds for inboxes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Inbox.delete_all
+
+5.times do
+  faker_name = Faker::Quote.famous_last_words
+  Inbox.create(name: faker_name)
+end
