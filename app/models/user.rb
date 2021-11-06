b4f77f08fbd3bb9f679108ca5b1fcce84e897020
Yaro <yshmarov@gmail.com>
2021-11-06 17:16:25 +0000

    user inboxes dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  has_many :inboxes
+
+  has_many :inboxes, dependent: :destroy
 end
