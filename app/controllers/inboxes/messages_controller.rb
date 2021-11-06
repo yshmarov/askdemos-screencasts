b3aebf3bd7a9dfe8a98e4a34e8db49e6d378b0be
Yaro <yshmarov@gmail.com>
2021-11-06 17:49:49 +0000

    rubocop

--- app/controllers/inboxes/messages_controller.rb
+++ app/controllers/inboxes/messages_controller.rb
@@ -1,36 +1,39 @@
-class Inboxes::MessagesController < ApplicationController
-  before_action :set_inbox
+module Inboxes
+  class MessagesController < ApplicationController
+    before_action :set_inbox
 
-  def new
-    @message = @inbox.messages.new
-  end
+    def new
+      @message = @inbox.messages.new
+    end
 
-  def create
-    @message = @inbox.messages.new(message_params)
+    def create
+      @message = @inbox.messages.new(message_params)
 
-    respond_to do |format|
-      if @message.save
-        format.html { redirect_to @inbox, notice: "Message was successfully created." }
-      else
-        format.html { render :new, status: :unprocessable_entity }
+      respond_to do |format|
+        if @message.save
+          format.html { redirect_to @inbox, notice: 'Message was successfully created.' }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+        end
       end
     end
-  end
 
-  def destroy
-    @message = Message.find(params[:id])
-    @message.destroy
-    respond_to do |format|
-      format.html { redirect_to @inbox, notice: "Message was successfully destroyed." }
+    def destroy
+      @message = Message.find(params[:id])
+      @message.destroy
+      respond_to do |format|
+        format.html { redirect_to @inbox, notice: 'Message was successfully destroyed.' }
+      end
     end
-  end
 
-  private
+    private
+
     def set_inbox
       @inbox = Inbox.find(params[:inbox_id])
     end
-  
+
     def message_params
       params.require(:message).permit(:body).merge(user: current_user)
     end
+  end
 end
