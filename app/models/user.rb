624c7e72815a69cb905e12ed4c439d64e0f30389
Yaro <yshmarov@gmail.com>
2021-11-07 16:43:45 +0000

    add devise confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
+         :confirmable,
          :omniauthable, omniauth_providers: [:github]
 
   has_many :inboxes, dependent: :destroy
