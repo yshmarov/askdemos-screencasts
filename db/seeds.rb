0e12924e17e35939a4f37caa926dd7aa7b3e903f
Yaro <yshmarov@gmail.com>
2021-11-07 04:46:30 +0000

    improving seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
+Message.delete_all
 Inbox.delete_all
+User.delete_all
 
-5.times do
-  faker_name = Faker::Quote.famous_last_words
-  Inbox.create(name: faker_name)
+rand(2..3).times do
+  faker_email = Faker::Internet.email
+  user = User.create(email: faker_email,
+                     password: Devise.friendly_token[0, 20])
+
+  rand(2..5).times do
+    faker_name = Faker::Quote.famous_last_words
+    inbox = Inbox.create(name: faker_name,
+                 user: user)
+
+    rand(2..5).times do
+      faker_body = Faker::Lorem.question
+      Message.create(body: faker_body,
+                     inbox: inbox,
+                     user: user)
+                     # TODO: make it a random user
+                     # add random votes
+    end
+  end
 end
+
+p "#{User.count} users added"
+p "#{Inbox.count} inboxes added"
+p "#{Message.count} messages added"
