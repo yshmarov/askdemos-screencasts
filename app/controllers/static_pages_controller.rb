a31e83f6be64cef71131a280a67cb17ff3eed1b0
secretpray <Renovatio100>
2021-11-12 09:45:25 +0200

    DRY set_meta_tags

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,17 +1,18 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :set_meta, only: %i[pricing privacy terms]
 
   def landing_page; end
 
-  def pricing
-    set_meta_tags title: action_name.capitalize
-  end
+  def pricing; end
 
-  def privacy
-    set_meta_tags title: action_name.capitalize
-  end
+  def privacy; end
 
-  def terms
+  def terms; end
+
+  private
+
+  def set_meta
     set_meta_tags title: action_name.capitalize
   end
 end
