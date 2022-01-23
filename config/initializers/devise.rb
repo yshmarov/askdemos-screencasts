5b3f778547781a451185634d0d34fe5538479240
Yaro <yshmarov@gmail.com>
2022-01-23 18:52:22 +0000

    update omniauth credentials

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,8 +273,8 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :github,
-    Rails.application.credentials.dig(:omniauth, :github, :id),
-    Rails.application.credentials.dig(:omniauth, :github, :secret)
+    Rails.application.credentials.dig(Rails.env.to_sym, :github, :client),
+    Rails.application.credentials.dig(Rails.env.to_sym, :github, :secret)
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
