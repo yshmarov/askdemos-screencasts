357eec7be9c55f8f3bdc6f51e67b4a51005eae32
Yaro <yshmarov@gmail.com>
2022-01-24 20:04:25 +0000

    leverage helper for better button names

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -4,18 +4,24 @@ module MessagesHelper
   end
 
   def upvote_label(message, user)
-    if user.voted_up_on? message, vote_scope: 'like'
-      'UN-vote'
-    else
-      'UP-vote'
+    vote_message = if user.voted_up_on? message, vote_scope: 'like'
+                     'UN-vote'
+                   else
+                     'UP-vote'
+                   end
+    tag.span do
+      "#{message.cached_scoped_like_votes_up} #{vote_message}"
     end
   end
 
   def downvote_label(message, user)
-    if user.voted_down_on? message, vote_scope: 'like'
-      'UN-vote'
-    else
-      'DOWN-vote'
+    vote_message = if user.voted_down_on? message, vote_scope: 'like'
+                     'UN-vote'
+                   else
+                     'DOWN-vote'
+                   end
+    tag.span do
+      "#{message.cached_scoped_like_votes_down} #{vote_message}"
     end
   end
 end
